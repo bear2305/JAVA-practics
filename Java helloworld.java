ad2c95d9fe8ad75bb60d00055a3a4f0a52b80f46
Physicslinux <emmanuelbanaheneadamnor@gmail.com>
2026-09-19 18:12:53 +0000

    Update Java helloworld.java

--- Java helloworld.java
+++ Java helloworld.java
@@ -1,5 +1,7 @@
 public class Main{
        public static void helloworld(String[] args){
              System.out.print("\nHello World\n"); 
+              //OR print with newline println
+             System.out.println("\n 2nd Hello World \n"); 
        }
 }
