f513e608288055db4e9f079bd68398b08faa39fe
Physicslinux <emmanuelbanaheneadamnor@gmail.com>
2026-09-24 23:30:20 +0000

    Fix variable declaration and update print statement

--- variables.java
+++ variables.java
@@ -1,13 +1,13 @@
 public class Main{
        public static void helloworld(String[] args){
               String name = "Banahene Emmanuel adamnor";
-              int index_number = 2425402521
+              int index_number = 2425402521;
               int age = 23;
-              String class = "computere science";
+              String classx = "computere science";
               float decimals = 234.99;
               System.out.print("\nHello World\n"); 
               //OR print with newline println
-              System.out.println(name,age,class,"\n"); 
+              System.out.println(name,age,classx,index_number,"\n"); 
               System.out.print(age+decimals);
 
 
