80e4b3c70d18452fd2d7ef962c8e629e2aef45fe
Physicslinux <emmanuelbanaheneadamnor@gmail.com>
2026-09-24 23:28:08 +0000

    Rename index variable and fix class declaration
    Renamed variable 'index' to 'index_num' and corrected 'class' declaration.

--- variables.java
+++ variables.java
@@ -1,7 +1,7 @@
 public class Main{
        public static void helloworld(String[] args){
               String name = "Banahene Emmanuel adamnor";
-              int index
+              int index_num
               int age = 23;
               String class = "computere science";
               float decimals = 234.99;
