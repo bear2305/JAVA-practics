aa1baa1a0698d30609a94f49e8d63fe2198c48ce
Physicslinux <emmanuelbanaheneadamnor@gmail.com>
2026-09-24 23:29:05 +0000

    Modify index_number and correct class declaration
    Updated index_number to a larger value and fixed a syntax error.

--- variables.java
+++ variables.java
@@ -1,7 +1,7 @@
 public class Main{
        public static void helloworld(String[] args){
               String name = "Banahene Emmanuel adamnor";
-              int index_number = 2425
+              int index_number = 2425402521
               int age = 23;
               String class = "computere science";
               float decimals = 234.99;
